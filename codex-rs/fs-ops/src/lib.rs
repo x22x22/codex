21a06c2db78d72aacfc59cd1bd058c21448100f5
Michael Bolin <mbolin@openai.com>
2026-03-19 15:38:44 -0700

    core: add a sandbox-backed fs helper

---
+++ codex-rs/fs-ops/src/lib.rs
@@ -0,0 +1,14 @@
+mod command;
+mod constants;
+mod error;
+mod runner;
+
+pub use command::FsCommand;
+pub use command::READ_FILE_OPERATION_ARG;
+pub use command::parse_command_from_args;
+pub use constants::CODEX_CORE_FS_OPS_ARG1;
+pub use error::FsError;
+pub use error::FsErrorKind;
+pub use runner::execute;
+pub use runner::run_from_args;
+pub use runner::write_error;
